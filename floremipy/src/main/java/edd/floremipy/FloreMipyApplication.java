115d8f6e2b24d5e721880ec8ee37034aee71a7cd
fdoueil <frederic.doueil@berger-levrault.com>
2017-06-02 15:45:17 +0200

    Init projet maven fabrique avec : https://start.spring.io/

---
+++ floremipy/src/main/java/edd/floremipy/FloreMipyApplication.java
@@ -0,0 +1,12 @@
+package edd.floremipy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloreMipyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloreMipyApplication.class, args);
+	}
+}
