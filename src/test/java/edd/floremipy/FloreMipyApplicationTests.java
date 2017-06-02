306c5c97ec34639a2d8b56079af356ed4eb85e04
fdoueil <frederic.doueil@berger-levrault.com>
2017-06-02 16:06:26 +0200

    Init 2 le retour

---
+++ src/test/java/edd/floremipy/FloreMipyApplicationTests.java
@@ -0,0 +1,16 @@
+package edd.floremipy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FloreMipyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
