c1be21934e0b827e1d98763ef4968f34d0b11bee
CAZAUX <sybille.cazaux@berger-levrault.fr>
2017-06-12 15:46:47 +0200

    préparation interface catalogue

---
+++ src/main/java/edd/floremipy/dao/CatalogueDAOImpl.java
@@ -0,0 +1,14 @@
+package edd.floremipy.dao;
+
+import java.util.Collections;
+import java.util.List;
+
+import edd.floremipy.dto.CatalogueLineDTO;
+
+public class CatalogueDAOImpl implements CatalogueDAOInterface {
+/* (non-Javadoc)
+ * @see edd.floremipy.dao.CatalogueDAOInterface#findCatalogue()
+ */
+@Override
+public List<CatalogueLineDTO> findCatalogue () {return Collections.emptyList();}
+}
