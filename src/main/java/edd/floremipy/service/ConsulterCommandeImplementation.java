83b6e8b97db0034af68318fc4f4ddeb2ceee420f
JFYVART <jean.francois.yvart@gmail.com>
2017-06-13 13:54:14 +0200

    Refactor : Déplacement du code mocké dans l'implémentation pour que la couche DTO soit propre.

--- src/main/java/edd/floremipy/service/ConsulterCommandeImplementation.java
+++ src/main/java/edd/floremipy/service/ConsulterCommandeImplementation.java
@@ -1,6 +1,7 @@
 package edd.floremipy.service;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 import edd.floremipy.dto.CommandListDTO;
 
@@ -15,8 +16,19 @@ public class ConsulterCommandeImplementation implements ConsulterCommandeInterfa
 	@Override
 	public ArrayList<CommandListDTO> initListeCommandeDuJour() {
 		CommandListDTO commandListDTO = new CommandListDTO();
+		this.fillWithMockedValue(commandListDTO);
 		this.listeInit.add(commandListDTO);
 		return this.listeInit;
+	}
+
+
+	protected void fillWithMockedValue(CommandListDTO item){
+		item.setId(-9999);
+		item.setClientName("Mon cLient 1");
+		item.setAdress("11 rue Berger Levrault - 31000 Toulouse");
+		item.setCommandReference("9999 - 99 -9999");
+		item.setTotalCommand(125.34d);
+		item.setDeliveryDate(new Date());
 	}
 
 }
