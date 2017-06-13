935497cfe892168d733ec7dc2f3595ae9b5ffa1a
peggyflo <peggy.flores@orange.fr>
2017-06-13 11:58:26 +0200

    JUnitTests sur CatalogueServiceImpl

--- src/main/java/edd/floremipy/service/CatalogueServiceImpl.java
+++ src/main/java/edd/floremipy/service/CatalogueServiceImpl.java
@@ -1,20 +1,33 @@
 package edd.floremipy.service;
 
 import java.util.ArrayList;
-import edd.floremipy.dto.CatalogueLineDTO;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-public class CatalogueServiceImpl implements CatalogueService {
+import edd.floremipy.dao.CatalogueDAOInterface;
+import edd.floremipy.dto.CatalogueLineDTO;
 
+@Service("catalogueService")
+public class CatalogueServiceImpl implements CatalogueService {
+	
+	@Autowired
+	CatalogueDAOInterface dao;
+	
+	
 	@Override
-	public String catalogLinesToJson(ArrayList<CatalogueLineDTO> myCatalogLines) {
+	public String catalogLinesToJson() {
+		
+		List<CatalogueLineDTO> myCatalogLines = dao.findCatalogue();
 		
 		Gson gson = new GsonBuilder().create();
 		StringBuffer sbLignesCatalogue = new StringBuffer();
 		
 		if (!(myCatalogLines.isEmpty())){
-
 			for (CatalogueLineDTO catalogueLineDTO : myCatalogLines) {
 				//convertir au format String JSON(P) chaque ligne du catalogue reçu
 				//( "id/ref - ArticleName - Category - Price - QtyStock")
