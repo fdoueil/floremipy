c1389ee964abb8a16135790cb52955a06d8b0c27
fdoueil <frederic.doueil@berger-levrault.com>
2017-06-30 13:48:41 +0200

    Roles et authentification, affichage des menus suivant le role utilisateur connecté

--- src/main/sql/user_roles.sql
+++ src/main/sql/user_roles.sql
@@ -5,8 +5,8 @@ CREATE TABLE `floremipy_tu`.`user_role` (
   `role` VARCHAR(45) NULL)
 COMMENT = 'user with roles';
 
-insert into user_role values (1,'le_directeur', 'Dir#01', 'DIRECTEUR');
-insert into user_role values (2,'le_jardinier', 'Jar#01', 'JARDINIER');
-insert into user_role values (3,'admin', 'admin', 'DIRECTEUR');
+insert into user_role values (1,'le_directeur', 'Dir#01', 'ROLE_DIRECTEUR');
+insert into user_role values (2,'le_jardinier', 'Jar#01', 'ROLE_JARDINIER');
+insert into user_role values (3,'admin', 'admin', 'ROLE_DIRECTEUR');
 insert into user_role values (4,'adm', 'adm', 'ROLE_ADMIN');
-insert into user_role values (5,'le_client', 'Cli#01', 'CLIENT');
+insert into user_role values (5,'le_client', 'Cli#01', 'ROLE_CLIENT');
