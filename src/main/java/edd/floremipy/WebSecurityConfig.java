a830e7ec9a4075bf263a078e1cf30f003ff0127f
fdoueil <frederic.doueil@berger-levrault.com>
2017-06-30 16:21:41 +0200

    Cryptage du mot de passe avec BCryptPasswordEncoder

--- src/main/java/edd/floremipy/WebSecurityConfig.java
+++ src/main/java/edd/floremipy/WebSecurityConfig.java
@@ -1,11 +1,14 @@
 package edd.floremipy;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import edd.floremipy.service.UserDetailsServiceImpl;
 
@@ -18,6 +21,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     
 	@Autowired
 	UserDetailsServiceImpl userDetailService;
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
+	}
 	
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -37,7 +45,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-    	auth.userDetailsService(userDetailService);
+    	auth.userDetailsService(userDetailService).passwordEncoder(passwordEncoder());
     }
     
 }
--- src/test/java/edd/floremipy/service/UserDetailsServiceImplTest.java
+++ src/test/java/edd/floremipy/service/UserDetailsServiceImplTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import edd.floremipy.dto.UserRoleDTO;
@@ -21,13 +22,16 @@ public class UserDetailsServiceImplTest {
 	@Autowired
 	private UserDetailsServiceImpl userDetailService;
 
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
 	@Test
 	public void testAdm() {
 		UserRoleDTO userRoleDto = (UserRoleDTO) userDetailService.loadUserByUsername("adm");
 		assertNotNull(userDetailService);
 		assertEquals(4, userRoleDto.getId());
 		assertTrue(isRolePresent(userRoleDto.getAuthorities(),"ROLE_ADMIN"));
-		assertEquals("adm",userRoleDto.getPassword());
+		assertTrue(passwordEncoder.matches("adm", "$2a$10$C9VKScI/geqTvEMqPLFWbeIcZczntJTE6umI2QJqHBS.JaVYD1mz2"));
 	}
 	
 	@Test(expected = UsernameNotFoundException.class)
