e92ab6aa53c620147163599692bdf42e37232bab
JFYVART <jean.francois.yvart@gmail.com>
2017-07-06 16:18:31 +0200

    Critique sonar

--- src/main/java/edd/floremipy/FloreMipyApplication.java
+++ src/main/java/edd/floremipy/FloreMipyApplication.java
@@ -6,19 +6,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
-
-import edd.floremipy.JpaConfiguration;
 
 @Import(JpaConfiguration.class)
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public class FloreMipyApplication {
-	
-	public static Date startDate=new Date();
+
+	static final Date startDate=new Date();
 
 	public static void main(String[] args) {
 		SpringApplication.run(FloreMipyApplication.class, args);
