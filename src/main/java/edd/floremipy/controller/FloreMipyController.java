6011124b3abaaed421c6acbab75b58afae658877
Bruno <bruno.demblans@berger-levrault.fr>
2017-07-06 15:41:16 +0200

    Suppr TAB et import inutile FloreMipyController

--- src/main/java/edd/floremipy/controller/FloreMipyController.java
+++ src/main/java/edd/floremipy/controller/FloreMipyController.java
@@ -3,7 +3,6 @@ package edd.floremipy.controller;
 import java.util.Collections;
 import java.util.List;
 
-import org.hibernate.mapping.Collection;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,14 +11,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class FloreMipyController {
-	
-	@RequestMapping(value = { "/", "/list" }, method = RequestMethod.GET)
-	public String catalogue(ModelMap model) {
+    
+    @RequestMapping(value = { "/", "/list" }, method = RequestMethod.GET)
+    public String catalogue(ModelMap model) {
 
-		List<Object> catalogue = Collections.emptyList();
-		model.addAttribute("catalogue", catalogue);
-		return "index";
-	}
+        List<Object> catalogue = Collections.emptyList();
+        model.addAttribute("catalogue", catalogue);
+        return "index";
+    }
 
 
 }
