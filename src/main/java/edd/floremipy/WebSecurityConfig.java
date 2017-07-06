6b80a98e09ef7edb459d3346ccbec41f57a83809
JFYVART <jean.francois.yvart@gmail.com>
2017-07-06 16:15:04 +0200

    Resolution Sonar Critique JFYV

--- src/main/java/edd/floremipy/WebSecurityConfig.java
+++ src/main/java/edd/floremipy/WebSecurityConfig.java
@@ -6,46 +6,46 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import edd.floremipy.service.UserDetailsServiceImpl;
 
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    
+
 	@Autowired
 	UserDetailsServiceImpl userDetailService;
-	
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-	    return new BCryptPasswordEncoder();
+		return new BCryptPasswordEncoder();
 	}
-	
-	@Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .antMatchers("/css/**").permitAll()
-                .antMatchers("/images/**").permitAll()
-                .antMatchers("/js/**").permitAll()
-                .antMatchers("/login**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin().loginPage("/login")
-                .and()
-                .logout().logoutUrl("/logout");
-        		http.csrf().disable();
-    }
 
-    @Autowired
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-    	auth.userDetailsService(userDetailService).passwordEncoder(passwordEncoder());
-    }
-    
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.authorizeRequests()
+		.antMatchers("/css/**").permitAll()
+		.antMatchers("/images/**").permitAll()
+		.antMatchers("/js/**").permitAll()
+		.antMatchers("/login**").permitAll()
+		.anyRequest().authenticated()
+		.and()
+		.formLogin().loginPage("/login")
+		.and()
+		.logout().logoutUrl("/logout");
+		http.csrf().disable();
+	}
+
+	@Autowired
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(this.userDetailService).passwordEncoder(this.passwordEncoder());
+	}
+
 }
