6b517574a32dcb2f563780e104ba03171a88415a
JFYVART <jean.francois.yvart@gmail.com>
2017-07-06 14:57:38 +0200

    Merge branch 'Formation JFYV'
    Conflicts:
    	pom.xml
    	src/main/java/edd/floremipy/WebSecurityConfig.java
    	src/main/java/edd/floremipy/controller/ArticleList.java
    	src/main/java/edd/floremipy/dao/UserRoleDAO.java
    	src/main/java/edd/floremipy/dto/UserDTO.java
    	src/main/java/edd/floremipy/service/UserServiceImpl.java
    	src/main/webapp/WEB-INF/menu.jsp

--- src/main/java/edd/floremipy/WebSecurityConfig.java
+++ src/main/java/edd/floremipy/WebSecurityConfig.java
@@ -4,38 +4,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import edd.floremipy.service.UserDetailsServiceImpl;
 
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    
+
 	@Autowired
 	UserDetailsServiceImpl userDetailService;
-	
-	@Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .antMatchers("/css/**").permitAll()
-                .antMatchers("/images/**").permitAll()
-                .antMatchers("/js/**").permitAll()
-                .antMatchers("/login**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin().loginPage("/login")
-                .and()
-                .logout().logoutUrl("/logout");
-        		http.csrf().disable();
-    }
 
-    @Autowired
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-    	auth.userDetailsService(userDetailService);
-    }
-    
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.authorizeRequests()
+		.antMatchers("/css/**").permitAll()
+		.antMatchers("/images/**").permitAll()
+		.antMatchers("/js/**").permitAll()
+		.antMatchers("/login**").permitAll()
+		.anyRequest().authenticated()
+		.and()
+		.formLogin().loginPage("/login")
+		.and()
+		.logout().logoutUrl("/logout");
+		http.csrf().disable();
+	}
+
+	@Autowired
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(this.userDetailService);
+	}
+
 }
--- src/main/java/edd/floremipy/dao/CustomerOrderDAOInterface.java
+++ src/main/java/edd/floremipy/dao/CustomerOrderDAOInterface.java
@@ -1,13 +1,10 @@
 package edd.floremipy.dao;
 
-import java.util.Date;
-
-import edd.floremipy.dto.CommandListDTO;
 import edd.floremipy.model.Customerorder;
-
+@FunctionalInterface
 public interface CustomerOrderDAOInterface {
 	// Retourne un numero de commande
 	public long ajouteCommande( Customerorder uneCommande );
-	
-	
+
+
 }
